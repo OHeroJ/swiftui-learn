bd8c5456df206151c00c24290e4274afc03893e6
laijihua <1164258202@qq.com>
2020-07-23 08:48:41 +0800

    Feat: 组件

---
+++ switui_learn/MapView.swift
@@ -0,0 +1,28 @@
+//
+//  MapView.swift
+//  switui_learn
+//
+//  Created by laijihua on 2020/7/23.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: Context) {
+        let coordinate = CLLocationCoordinate2D(latitude: 34, longitude: -116)
+        let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+        let region = MKCoordinateRegion(center: coordinate, span: span)
+        uiView.setRegion(region, animated: true)
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
