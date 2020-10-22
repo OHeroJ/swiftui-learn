bf3988a8200f987b7873d09953accd744790bfeb
laijihua <1164258202@qq.com>
2020-10-22 11:36:11 +0800

    Feat: GeometryReader

--- switui_learn/Learn/LearnGeometryReader.swift
+++ switui_learn/Learn/LearnGeometryReader.swift
@@ -10,7 +10,6 @@ import SwiftUI
 struct LearnGeometryReader: View {
     var body: some View {
         VStack {
-
             ScrollView(.horizontal, showsIndicators: false, content: {
                 HStack(spacing: 50) {
                     ForEach(1..<8) { item in
@@ -23,11 +22,32 @@ struct LearnGeometryReader: View {
                                 .shadow(color: Color.gray, radius: 10, x: 0, y: 0)
                                 .rotation3DEffect(Angle(degrees: Double(geometry.frame(in: .global).minX - 20) / 40), axis: (x: 20, y: 20, z: 20))
                         }
-                        .frame(width: 370, height: 400)
+                        .frame(width: 270, height: 300)
                         .padding(20)
                     }
                 }
             })
+            
+            Circle().fill(Color.blue)
+                .overlay(Circle().strokeBorder(Color.white).padding(10))
+                .frame(width: 75, height: 75, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            
+            Divider()
+            
+            Text("love swiftui")
+                .foregroundColor(.white)
+                .padding(10)
+                .background(GeometryReader{ proxy in
+                Rectangle()
+                    .fill(Color.blue)
+                    .frame(width: proxy.size.width, height: proxy.size.height)
+            })
+            
+            Text("love swiftui")
+                .foregroundColor(.white)
+                .padding(10)
+                .background(Color.blue)
+            
         }.navigationTitle(Text("GeometryReader"))
     }
 }
